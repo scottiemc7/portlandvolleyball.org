6f976e8e0eadceb1a24ab09392cbc135f190419d
David Rueck <drueck@gmail.com>
2015-08-10 13:59:12 -0700

    Update outdoor rules.

--- pvaoutdoorrules.php
+++ pvaoutdoorrules.php
@@ -2,39 +2,49 @@
 
 <h1>Portland Volleyball Association - Outdoor Playing Regulations</h1>
 <p>
-<i>All leagues will play with outdoor rules including:</i><br />
+<i>Outdoor Rules for Sand and Grass Leagues</i><br />
 <ul>
-<li>Rally scoring with 2 sets to 21 points (no cap, win by 2) and third set to 15 (no cap, win by 2)</li>
-<li>Sets 1 & 2 switch sides every 7 points and set 3 switch sides every 5 points</li>
-<li>First and third sets begin with a coin toss (or ball spin or ...) to determine which team gets first choice of serve or side</li>
-<li>For the second set, the team that lost the "coin toss" gets first choice of serve or side</li>
-<li>No open hand tips or dinks (cobra or knuckles are allowed)</li>
-<li>Serve receive cannot be with open hands (i.e. set)</li>
-<li>Hard driven hit can be set but off-speed hit cannot be set</li>
-<li>No directional blocks</li>
-<li>Let serves are allowed but not re-tosses</li>
-<li>No rotational faults, but must keep service order</li>
-<li>Serving order must alternate male and female</li>
-<li>Only contact with the top tape of the net is a fault</li>
-<li>Posts are considered antennas</li>
-<li>No center line violation unless you interfere with player on other side of court</li>
-<li>No woman touch rule for multiple touches</li>
-<li>2 players contacting the ball at the same time is considered 2 hits and either player can then take it over the net</li>
-<li>If setting the ball over the net, the set must be perpendicular with your shoulders (either front or back set)</li>
-<li>A block counts as the first touch so only 2 more touches are allowed</li>
-<li>Doubles played on short courts; all others on standard courts</li>
-<li>One 60 second timeout per set</li>
-<li>5 minute warm-up unless both teams played in a preceding match then 3 minute warm-up</li>
-<li>Replay if a tree branch interferes with a playable ball</li>
+  <li>Rally scoring with 2 sets to 21 points (no cap, win by 2) and third set to 15 (no cap, win by 2)</li>
+  <li>Sets 1 &amp; 2 switch sides every 7 points and set 3 switch sides every 5 points</li>
+  <li>First and third sets begin with a coin toss (or ball spin or ...) to determine which team gets first choice of serve or side</li>
+  <li>For the second set, the team that lost the "coin toss" gets first choice of serve or side</li>
+  <li>A block counts as the first touch so only 2 more touches are allowed</li>
+  <li>No open hand tips or dinks (cobra or knuckles are allowed)</li>
+  <li>No woman touch rule for multiple touches</li>
+  <li>Serve receive cannot be with open hands (i.e. set)</li>
+  <li>Coed serving order must alternate male and female</li>
+  <li>No rotational faults, but must keep service order</li>
+  <li>Let serves are allowed but not re-tosses</li>
+  <li><u>Hard driven</u> hit can be set (official's judgement) but off-speed hit cannot be set (no finger action until 2nd or 3rd contact)</li>
+  <li>No directional blocks (no breaking wrist, either down or to a side)</li>
+  <li>Only contact with the top tape of the net is a fault</li>
+  <li>Posts are considered antennas</li>
+  <li>No center line violation unless you interfere with player on other side of court</li>
+  <li>2 players contacting the ball at the same time is considered 2 hits and either player can then take it over the net unless referee clearly sees only one person contacting the ball</li>
+  <li>If setting the ball over the net, the set must be perpendicular with your shoulders (either front or back set)</li>
+  <li>One 60 second timeout per set</li>
+  <li>Replay if a tree branch interferes with a playable ball</li>
+  <li>Reminder, no alcohol permitted before or during play</li>
 </ul>
 </p>
 <p>
-<i>Additional rules for Reverse Coed</i><br />
+<i>Additional Rules for Reverse Coed</i><br />
 <ul>
-<li>Men must take off behind 10 foot line to hit a ball with a downward flight</li>
-<li>Men may contact the ball in front of the 10 foot line as long as it has an upward flight</li>
-<li>Men may not block unless ball stays on blocker's side of net (i.e. soft block) - counts as the first touch</li>
+  <li>Men must take off behind 10 foot line to hit a ball with a downward flight</li>
+  <li>Men may contact the ball in front of the 10 foot line as long as it has an upward flight</li>
+  <li>Men may not block unless ball stays on blocker's side of net (i.e. soft block) - counts as the first touch</li>
+  <li>Serving order must alternate male and female</li>
 </ul>
 </p>
+<p>
+<i>Additional Rules for Sand</i><br>
+<ul>
+  <li>Same as outdoor rules except teams playing with 5 or 6 players must use the following indoor rules:
+    <ul>
+      <li>Players must line up alternating male and female and rotate accordingly</li>
+      <li>A woman must touch the ball if there is more than one contact</li>
+    </ul>
+  </li>
+</ul>
 
 <?php include("footer.html"); ?>
