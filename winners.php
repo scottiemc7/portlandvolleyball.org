a1998446a93e273f6e2f6c3fd2afcc364db046a0
David Rueck <drueck@gmail.com>
2016-02-10 12:45:56 -0800

    Add Fall 2015 Playoff Winners and remove extraneous p tags around uls

--- winners.php
+++ winners.php
@@ -1,133 +1,132 @@
 <?php include("header.html"); ?>
 
+<h1>Fall 2015 Playoff Winners</h1>
+<ul>
+  <li><strong>Women&#39;s A1</strong> Team North</li>
+  <li><strong>Women&#39;s A2</strong> Vital Force</li>
+  <li><strong>Women&#39;s B</strong> All About the Ace</li>
+  <li><strong>Women&#39;s BB</strong> You've Been Served</li>
+  <li><strong>Women&#39;s AA</strong> Spiked Punch </li>
+  <li><strong>Women&#39;s A</strong> Smack That Pass</li>
+  <li><strong>Coed B1</strong> East of Eden</li>
+  <li><strong>Coed B2</strong> Two Dink Minimum</li>
+  <li><strong>Coed C</strong> Mudsharks </li>
+  <li><strong>Coed A Thursday</strong> PVA All Stars</li>
+  <li><strong>Coed B Thursday</strong> The Bald and the Beautiful</li>
+</ul>
+
 <h1>Summer 2015 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women&#39;s A1</strong> Shin-Digs</li>
-    <li><strong>Women&#39;s A2</strong> Deflated Balls</li>
-    <li><strong>Coed Reverse Quads A</strong> PVA All-Stars</li>
-    <li><strong>Coed Reverse Quads B</strong> iSpike</li>
-    <li><strong>Sand</strong> Sunshine</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women&#39;s A1</strong> Shin-Digs</li>
+  <li><strong>Women&#39;s A2</strong> Deflated Balls</li>
+  <li><strong>Coed Reverse Quads A</strong> PVA All-Stars</li>
+  <li><strong>Coed Reverse Quads B</strong> iSpike</li>
+  <li><strong>Sand</strong> Sunshine</li>
+</ul>
 
 <h1>Spring 2015 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women&#39;s A1</strong> Notorious DIG</li>
-    <li><strong>Women&#39;s A2</strong> Team North </li>
-    <li><strong>Women&#39;s BB 1</strong> Spiked Punch</li>
-    <li><strong>Women&#39;s BB 2</strong> The Guidettes</li>
-    <li><strong>Women&#39;s B</strong> We're Out of Shape </li>
-    <li><strong>Women&#39;s AA</strong> Hot Spicy Ballz </li>
-    <li><strong>Women&#39;s A Tuesdays</strong> Safe Sets </li>
-    <li><strong>Women&#39;s A Wednesday</strong> Twice Around the Block</li>
-    <li><strong>Coed A</strong> The Servicals</li>
-    <li><strong>Coed B</strong> Booya </li>
-    <li><strong>Coed C</strong> A &amp; L Bombers </li>
-    <li><strong>Coed A Thursday</strong> Court Jesters </li>
-    <li><strong>Coed B Thursday</strong> Top Gun</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women&#39;s A1</strong> Notorious DIG</li>
+  <li><strong>Women&#39;s A2</strong> Team North </li>
+  <li><strong>Women&#39;s BB 1</strong> Spiked Punch</li>
+  <li><strong>Women&#39;s BB 2</strong> The Guidettes</li>
+  <li><strong>Women&#39;s B</strong> We're Out of Shape </li>
+  <li><strong>Women&#39;s AA</strong> Hot Spicy Ballz </li>
+  <li><strong>Women&#39;s A Tuesdays</strong> Safe Sets </li>
+  <li><strong>Women&#39;s A Wednesday</strong> Twice Around the Block</li>
+  <li><strong>Coed A</strong> The Servicals</li>
+  <li><strong>Coed B</strong> Booya </li>
+  <li><strong>Coed C</strong> A &amp; L Bombers </li>
+  <li><strong>Coed A Thursday</strong> Court Jesters </li>
+  <li><strong>Coed B Thursday</strong> Top Gun</li>
+</ul>
 
 <h1>Winter 2015 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women&#39;s A1</strong> Notorious DIG</li>
-    <li><strong>Women&#39;s A2</strong> Volley Patron </li>
-    <li><strong>Women&#39;s BB 1</strong> Baby Got Backset</li>
-    <li><strong>Women&#39;s BB 2</strong> Crazy Eights </li>
-    <li><strong>Women&#39;s AA</strong> Mavens</li>
-    <li><strong>Women&#39;s A Tuesdays</strong> Sneaker Wave</li>
-    <li><strong>Women&#39;s A Wednesday</strong> Vital Force</li>
-    <li><strong>Coed A</strong> Alpine Spikers </li>
-    <li><strong>Coed B</strong> Yes Yes</li>
-    <li><strong>Coed C</strong> Vertically Challenged</li>
-    <li><strong>Coed A Thursday</strong> Jello Shots</li>
-    <li><strong>Coed B Thursday</strong> Orville Ready Blockers</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women&#39;s A1</strong> Notorious DIG</li>
+  <li><strong>Women&#39;s A2</strong> Volley Patron </li>
+  <li><strong>Women&#39;s BB 1</strong> Baby Got Backset</li>
+  <li><strong>Women&#39;s BB 2</strong> Crazy Eights </li>
+  <li><strong>Women&#39;s AA</strong> Mavens</li>
+  <li><strong>Women&#39;s A Tuesdays</strong> Sneaker Wave</li>
+  <li><strong>Women&#39;s A Wednesday</strong> Vital Force</li>
+  <li><strong>Coed A</strong> Alpine Spikers </li>
+  <li><strong>Coed B</strong> Yes Yes</li>
+  <li><strong>Coed C</strong> Vertically Challenged</li>
+  <li><strong>Coed A Thursday</strong> Jello Shots</li>
+  <li><strong>Coed B Thursday</strong> Orville Ready Blockers</li>
+</ul>
 
 <h1>Fall 2014 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women&#39;s A</strong> Family Affair</li>
-    <li><strong>Women&#39;s BB 1</strong> Chickaboom</li>
-    <li><strong>Women&#39;s BB 2</strong> Glitter Gang</li>
-    <li><strong>Women&#39;s AA</strong> Spiked Punch</li>
-    <li><strong>Women&#39;s A Tuesdays</strong> Vital Force</li>
-    <li><strong>Women&#39;s B</strong> Serves You Right</li>
-    <li><strong>Coed A</strong> Mo Diggity</li>
-    <li><strong>Coed B</strong> Yes Yes</li>
-    <li><strong>Coed C</strong> Vertically Challenged</li>
-    <li><strong>Coed A Thursday</strong> Hop Heads</li>
-    <li><strong>Coed B Thursday</strong> Carry On</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women&#39;s A</strong> Family Affair</li>
+  <li><strong>Women&#39;s BB 1</strong> Chickaboom</li>
+  <li><strong>Women&#39;s BB 2</strong> Glitter Gang</li>
+  <li><strong>Women&#39;s AA</strong> Spiked Punch</li>
+  <li><strong>Women&#39;s A Tuesdays</strong> Vital Force</li>
+  <li><strong>Women&#39;s B</strong> Serves You Right</li>
+  <li><strong>Coed A</strong> Mo Diggity</li>
+  <li><strong>Coed B</strong> Yes Yes</li>
+  <li><strong>Coed C</strong> Vertically Challenged</li>
+  <li><strong>Coed A Thursday</strong> Hop Heads</li>
+  <li><strong>Coed B Thursday</strong> Carry On</li>
+</ul>
 
 <h1>Summer 2014 Winners</h1>
-<p>
-  <ul>
-    <li><strong>Sand</strong> Summer Fun</li>
-    <li><strong>Coed Reverse Quads</strong> PVA All Stars</li>
-    <li><strong>Women's Grass A Quads</strong> Mavens</li>
-    <li><strong>Women's Grass B Quads</strong> Be Minderful</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Sand</strong> Summer Fun</li>
+  <li><strong>Coed Reverse Quads</strong> PVA All Stars</li>
+  <li><strong>Women's Grass A Quads</strong> Mavens</li>
+  <li><strong>Women's Grass B Quads</strong> Be Minderful</li>
+</ul>
 
 <h1>Spring 2014 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women's B</strong> Serve-ivors</li>
-    <li><strong>Women's A</strong> Safe Sets</li>
-    <li><strong>Women's BB1</strong> #cantstop #wontstop</li>
-    <li><strong>Women's BB2</strong> Over the Net</li>
-    <li><strong>Women's AA</strong> Mavens</li>
-    <li><strong>Women's A TUES</strong> Like a Boss</li>
-    <li><strong>Coed A</strong> The Servicals</li>
-    <li><strong>Coed B</strong> Spiked Punch</li>
-    <li><strong>Coed C</strong> A &amp; L Bombers</li>
-    <li><strong>Coed A Thurs</strong> Whatever</li>
-    <li><strong>Coed B Thurs</strong> Top Gun</li>
-    <li><strong>Coed C Thurs</strong> The Empire Spikes Back</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women's B</strong> Serve-ivors</li>
+  <li><strong>Women's A</strong> Safe Sets</li>
+  <li><strong>Women's BB1</strong> #cantstop #wontstop</li>
+  <li><strong>Women's BB2</strong> Over the Net</li>
+  <li><strong>Women's AA</strong> Mavens</li>
+  <li><strong>Women's A TUES</strong> Like a Boss</li>
+  <li><strong>Coed A</strong> The Servicals</li>
+  <li><strong>Coed B</strong> Spiked Punch</li>
+  <li><strong>Coed C</strong> A &amp; L Bombers</li>
+  <li><strong>Coed A Thurs</strong> Whatever</li>
+  <li><strong>Coed B Thurs</strong> Top Gun</li>
+  <li><strong>Coed C Thurs</strong> The Empire Spikes Back</li>
+</ul>
 
 <h1>Winter 2014 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women's B</strong> Floor Burn</li>
-    <li><strong>Women's A</strong> Sneaker Boss</li>
-    <li><strong>Women's BB1</strong> Vital Force</li>
-    <li><strong>Women's BB2</strong> #cantstop #wontstop</li>
-    <li><strong>Women's B TUES</strong> Dig This</li>
-    <li><strong>Women's AA</strong> Harvest Honeys </li>
-    <li><strong>Women's A TUES</strong> Sneaker Wave</li>
-    <li><strong>Coed A</strong> Court Jesters</li>
-    <li><strong>Coed B</strong> Spiked Punch</li>
-    <li><strong>Coed C</strong> Block Party</li>
-    <li><strong>Coed A Thurs</strong> 4/10 8 &amp; 9 pm PCC</li>
-    <li><strong>Coed B Thurs</strong> Team Pancake</li>
-    <li><strong>Coed C Thurs</strong> Spikerz</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women's B</strong> Floor Burn</li>
+  <li><strong>Women's A</strong> Sneaker Boss</li>
+  <li><strong>Women's BB1</strong> Vital Force</li>
+  <li><strong>Women's BB2</strong> #cantstop #wontstop</li>
+  <li><strong>Women's B TUES</strong> Dig This</li>
+  <li><strong>Women's AA</strong> Harvest Honeys </li>
+  <li><strong>Women's A TUES</strong> Sneaker Wave</li>
+  <li><strong>Coed A</strong> Court Jesters</li>
+  <li><strong>Coed B</strong> Spiked Punch</li>
+  <li><strong>Coed C</strong> Block Party</li>
+  <li><strong>Coed A Thurs</strong> 4/10 8 &amp; 9 pm PCC</li>
+  <li><strong>Coed B Thurs</strong> Team Pancake</li>
+  <li><strong>Coed C Thurs</strong> Spikerz</li>
+</ul>
 
 <h1>Fall 2013 Playoff Winners</h1>
-<p>
-  <ul>
-    <li><strong>Women's B</strong>You've Been Served </li>
-    <li><strong>Women's A</strong> We Could Carry Less</li>
-    <li><strong>Women's BB</strong> Balls Out</li>
-    <li><strong>Women's B TUES</strong> SERVE-IVORS</li>
-    <li><strong>Women's AA</strong> Mavens</li>
-    <li><strong>Women's A TUES</strong> Angry Birds</li>
-    <li><strong>Coed A</strong> The Panthers</li>
-    <li><strong>Coed B</strong> The Tip</li>
-    <li><strong>Coed C</strong> Vertically Challenged</li>
-    <li><strong>Coed A Thurs</strong> Whatever</li>
-    <li><strong>Coed B Thurs</strong> Spider Monkeys</li>
-    <li><strong>Coed C Thurs</strong> Volley Llama</li>
-  </ul>
-</p>
+<ul>
+  <li><strong>Women's B</strong>You've Been Served </li>
+  <li><strong>Women's A</strong> We Could Carry Less</li>
+  <li><strong>Women's BB</strong> Balls Out</li>
+  <li><strong>Women's B TUES</strong> SERVE-IVORS</li>
+  <li><strong>Women's AA</strong> Mavens</li>
+  <li><strong>Women's A TUES</strong> Angry Birds</li>
+  <li><strong>Coed A</strong> The Panthers</li>
+  <li><strong>Coed B</strong> The Tip</li>
+  <li><strong>Coed C</strong> Vertically Challenged</li>
+  <li><strong>Coed A Thurs</strong> Whatever</li>
+  <li><strong>Coed B Thurs</strong> Spider Monkeys</li>
+  <li><strong>Coed C Thurs</strong> Volley Llama</li>
+</ul>
 
 <?php include("footer.html"); ?>
