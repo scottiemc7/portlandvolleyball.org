5fa8d7dee86f06e2136211ab067eed4da9edf3d1
Scott McClure <scottmcclure@Scotts-MBP.lan>
2021-04-25 12:20:16 -0700

    Update to About. Changing leagues and other details to be current-ish.

--- about.php
+++ about.php
@@ -11,11 +11,11 @@
 			community centers all over Portland.</p>
 
 			<p>Everybody is welcome to play in the Portland City League!  We have leagues for all skill levels:
-			Coed A-G, Mens A and B, and Womens A and B.  Summer leagues are:  Coed Grass 4s Reverse, Coed Grass
+			Coed A, B, and C, and Womens AA, A, BB, and B.  Summer leagues are:  Coed Grass 4s Reverse, Coed Grass
 			Doubles Reverse, Grass Coed 6s, Grass Womens 4s, Sand 4s, and Sand 6s.</p>
 
 			<h4>PVA Board</h4>
-			<p>Portland Volleyball Association is managed by an all-volunteer board and one paid executive assistant.  The
+			<p>Portland Volleyball Association is managed by an all-volunteer board, a paid scheduling manager, and a paid communications manager.  The
 			board meets approximately every 5-6 weeks to discuss all aspects of running the league.  The current
 			board members are as follows:</p>
 			<table class="table table-striped table-bordered">
