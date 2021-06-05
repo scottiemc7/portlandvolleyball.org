ef67c378a302ff7585f17e9649757b63d5917f93
Scott McClure <scottie.mcclure@gmail.com>
2021-06-05 15:54:57 -0700

    Updating check in help

--- help.php
+++ help.php
@@ -1,5 +1,21 @@
 <?php include 'header.html.php'; ?>
 <div id="content" class="container">
-<h1> HI! </h1>
+<h1>COVID Check-In Procedure</h1>
+
+<ol type="1">
+  <li>
+    Make sure you have a PVA account and are on a team roster. If you need to sign up for an account, please register here:
+    <a href="https://register.portlandvolleyball.org/account/signup">https://register.portlandvolleyball.org/account/signup</a>
+  </li>
+  <li>
+    Scan the QR code at the facility or click on a link here:
+    <a href="https://register.portlandvolleyball.org/checkin/facility/2">Delta Park</a>
+    <a href="https://register.portlandvolleyball.org/checkin/facility/3">Brentwood Park</a>
+  <li>
+
+  <li>Sign in to your account if you're prompted to.<li>
+  <li>Show your check-in screen to the referee on site before stepping on the court.</li>
+
+</ol>
 
 <?php include("footer.html.php"); ?>
