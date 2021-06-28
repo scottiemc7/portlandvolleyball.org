19930099fbebba3b40596e5a02bc676f1f586270
Scott McClure <scottie.mcclure@gmail.com>
2021-06-28 07:41:14 -0700

    Testing login

--- schedules.php
+++ schedules.php
@@ -1,3 +1,21 @@
 <?php
-include 'schedules_secure.php';
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+if($user == "admin" && $pass == "admin")
+{
+    include 'schedules_secure.php';
+}
+else
+{
+    if(isset($_POST))
+    {?>
+
+            <form method="POST" action="secure.php">
+            User <input type="text" name="user"></input><br/>
+            Pass <input type="password" name="pass"></input><br/>
+            <input type="submit" name="submit" value="Go"></input>
+            </form>
+    <?}
+}
 ?>
