6401a1e89cb8a1f68597ecaec77a5bc7399a96c3
Scott McClure <scottie.mcclure@gmail.com>
2021-06-28 07:59:46 -0700

    Trying cookie option

--- schedules.php
+++ schedules.php
@@ -1,5 +1,23 @@
 <?php
-$pass = $_POST['pass'];
-include 'schedules_secure.php';
+if(isset($_COOKIE['ScheduleLogin']))
+{
+    include 'schedules_secure.php';
+}
+else
+{
+    $pass = $_POST['pass'];
 
+    if($pass == "pvasummer123")
+    {
+        setcookie('ScheduleLogin', md5($_POST['pass']));
+
+    }
+    else if(isset($_POST))
+    {?>
+        <form method="POST" action="schedules.php">
+        Password <input type="password" name="pass"></input><br/>
+        <input type="submit" name="submit" value="Submit"></input>
+        </form>
+    <?}
+}
 ?>
