d1bf209de25486ef0ca564f4c013b690392814f0
Scott McClure <scottie.mcclure@gmail.com>
2021-06-28 07:51:35 -0700

    Testing filters

--- schedules.php
+++ schedules.php
@@ -1,19 +1,19 @@
 <?php
 $pass = $_POST['pass'];
+include 'schedules_secure.php';
+// if($pass == "pvasummer123")
+// {
+//     include 'schedules_secure.php';
+// }
+// else
+// {
+//     if(isset($_POST))
+//     {?>
 
-if($pass == "pvasummer123")
-{
-    include 'schedules_secure.php';
-}
-else
-{
-    if(isset($_POST))
-    {?>
-
-            <form method="POST" action="schedules.php">
-            Password <input type="password" name="pass"></input><br/>
-            <input type="submit" name="submit" value="Submit"></input>
-            </form>
-    <?}
-}
+//             <form method="POST" action="schedules.php">
+//             Password <input type="password" name="pass"></input><br/>
+//             <input type="submit" name="submit" value="Submit"></input>
+//             </form>
+//     <?}
+// }
 ?>
