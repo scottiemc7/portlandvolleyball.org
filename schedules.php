a31e6b25cd3dfbf998245e740270596903952477
Scott McClure <scottie.mcclure@gmail.com>
2021-06-28 07:47:05 -0700

    Fixing layout and changing password for schedules

--- schedules.php
+++ schedules.php
@@ -1,9 +1,7 @@
-<?php include 'header.html.php'; ?>
-<div id="content" class="container">
 <?php
 $pass = $_POST['pass'];
 
-if($pass == "admin")
+if($pass == "pvasummer123")
 {
     include 'schedules_secure.php';
 }
@@ -14,10 +12,8 @@ else
 
             <form method="POST" action="schedules.php">
             Password <input type="password" name="pass"></input><br/>
-            <input type="submit" name="submit" value="Go"></input>
+            <input type="submit" name="submit" value="Submit"></input>
             </form>
     <?}
 }
 ?>
-</div>
-<?php include("footer.html.php"); ?>
