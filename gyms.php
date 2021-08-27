c5f5c4dd8f4e0a1655b9c36ab0ca766151b3bf75
Scott McClure <scottie.mcclure@gmail.com>
2021-08-26 21:22:18 -0700

    Removing password from gyms page

--- gyms.php
+++ gyms.php
@@ -1,23 +1,26 @@
 <?php
-if(isset($_COOKIE['ScheduleLoginSummer']))
-{
+//if we need to password protect this again, here is the code.
+// if(isset($_COOKIE['ScheduleLoginSummer']))
+// {
     include 'gyms_secure.php';
-}
-else
-{
-    $pass = $_POST['pass'];
+// }
+// else
+// {
+//     $pass = $_POST['pass'];
 
-    if($pass == "pvasummer123")
-    {
-        setcookie('ScheduleLoginSummer', md5($_POST['pass']));
-        header("Location: $_SERVER[PHP_SELF]");
-    }
-    else if(isset($_POST))
-    {?>
-        <form method="POST" action="gyms.php">
-        Password <input type="password" name="pass"></input><br/>
-        <input type="submit" name="submit" value="Submit"></input>
-        </form>
-    <?}
-}
+//     if($pass == "pvasummer123")
+//     {
+//         setcookie('ScheduleLoginSummer', md5($_POST['pass']));
+//         header("Location: $_SERVER[PHP_SELF]");
+//     }
+//     else if(isset($_POST))
+//     {
+//   ?>
+//         <form method="POST" action="gyms.php">
+//         Password <input type="password" name="pass"></input><br/>
+//         <input type="submit" name="submit" value="Submit"></input>
+//         </form>
+//     <?
+//}
+// }
 ?>
