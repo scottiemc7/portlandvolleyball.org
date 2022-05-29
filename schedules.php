0fe1832b940adacfc332efe1a6c3229a57fcee3b
Scott McClure <scottie.mcclure@gmail.com>
2022-05-29 13:20:33 -0700

    Password protecting schedules page

--- schedules.php
+++ schedules.php
@@ -1,25 +1,25 @@
 <?php
-// if(isset($_COOKIE['ScheduleLoginSummer']))
-// {
+if(isset($_COOKIE['ScheduleLoginSummer']))
+{
     include 'schedules_secure.php';
-// }
-// else
-// {
-//     $pass = $_POST['pass'];
+}
+else
+{
+    $pass = $_POST['pass'];
 
-//     if($pass == "pvasummer123")
-//     {
-//         setcookie('ScheduleLoginSummer', md5($_POST['pass']));
-//         header("Location: $_SERVER[PHP_SELF]");
-//     }
-//     else if(isset($_POST))
-//     {
+    if($pass == "pvasummer123")
+    {
+        setcookie('ScheduleLoginSummer', md5($_POST['pass']));
+        header("Location: $_SERVER[PHP_SELF]");
+    }
+    else if(isset($_POST))
+    {
    ?>
-         <!-- <form method="POST" action="schedules.php">
+         <form method="POST" action="schedules.php">
          Password <input type="password" name="pass"></input><br/>
          <input type="submit" name="submit" value="Submit"></input>
-         </form>-->
+         </form>
      <?
-//      }
-// }
+     }
+}
 ?>
