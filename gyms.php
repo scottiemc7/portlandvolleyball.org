a082a885ae8fdb7b03e7aad49256fe31576594d2
Scott McClure <scottie.mcclure@gmail.com>
2022-06-20 18:10:16 -0700

    Hiding gyms page

--- gyms.php
+++ gyms.php
@@ -1,26 +1,26 @@
 <?php
 //if we need to password protect this again, here is the code.
-// if(isset($_COOKIE['ScheduleLoginSummer']))
-// {
+if(isset($_COOKIE['ScheduleLoginSummer']))
+{
     include 'gyms_secure.php';
-// }
-// else
-// {
-//     $pass = $_POST['pass'];
+}
+else
+{
+    $pass = $_POST['pass'];
 
-//     if($pass == "pvasummer123")
-//     {
-//         setcookie('ScheduleLoginSummer', md5($_POST['pass']));
-//         header("Location: $_SERVER[PHP_SELF]");
-//     }
-//     else if(isset($_POST))
-//     {
+    if($pass == "pvasummer123")
+    {
+        setcookie('ScheduleLoginSummer', md5($_POST['pass']));
+        header("Location: $_SERVER[PHP_SELF]");
+    }
+    else if(isset($_POST))
+    {
    ?>
-        <!-- <form method="POST" action="gyms.php">
+        <form method="POST" action="gyms.php">
          Password <input type="password" name="pass"></input><br/>
          <input type="submit" name="submit" value="Submit"></input>
-         </form> -->
+         </form>
      <?
-//}
-// }
+}
+}
 ?>
